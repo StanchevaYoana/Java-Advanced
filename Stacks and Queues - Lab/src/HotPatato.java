041ea123564735ba570c8d2ff4a867574a93cb97
StanchevaYoana <yoana.radoslavova@gmail.com>
2019-07-22 16:06:42 +0300

    Add task - Hot patato .

---
+++ Stacks and Queues - Lab/src/HotPatato.java
@@ -0,0 +1,24 @@
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class HotPatato {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayDeque<String> queue = Arrays.stream(scanner.nextLine()
+                .split("\\s+")).collect(Collectors.toCollection(ArrayDeque::new));
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        while (queue.size() > 1) {
+            for (int i = 1; i < n; i++) {
+                queue.offer(queue.poll());
+            }
+            System.out.println("Removed "+ queue.pop());
+        }
+        System.out.println("Last is " + queue.peek());
+    }
+}
+
