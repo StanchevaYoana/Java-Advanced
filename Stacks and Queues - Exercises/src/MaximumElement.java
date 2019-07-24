087184fcb9da41085c196497803fe74b9fe76f54
StanchevaYoana <yoana.radoslavova@gmail.com>
2019-07-24 16:48:51 +0300

    Add task - Maximum element .

--- Stacks and Queues - Exercises/src/MaximumElement.java
+++ Stacks and Queues - Exercises/src/MaximumElement.java
@@ -1,5 +1,29 @@
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class MaximumElement {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
 
+        for (int i = 0; i < n; i++) {
+            int[] input = Arrays.stream(scanner.nextLine()
+                    .split(" ")).mapToInt(Integer::parseInt).toArray();
+
+            switch (input[0]) {
+                case 1:
+                    stack.push(input[1]);
+                    break;
+                case 2:
+                    stack.pop();
+                    break;
+                case 3:
+                    System.out.println(Collections.max(stack));
+                    break;
+            }
+        }
     }
 }
