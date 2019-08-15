e0c7fae0b96409897a79e75144bbac86e3a4002b
StanchevaYoana <yoana.radoslavova@gmail.com>
2019-08-15 15:40:23 +0300

    Add task - Copy bytes .

---
+++ Streams, Files And Directories - Lab/src/CopyBytes.java
@@ -0,0 +1,36 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class CopyBytes {
+    public static void main(String[] args) {
+
+        String pathIn = System.getProperty("user.dir") + "/res/input.txt";
+        String pathOut = System.getProperty("user.dir") + "/res/output2.txt";
+
+        try (FileInputStream fis = new FileInputStream(pathIn);
+             FileOutputStream fos = new FileOutputStream(pathOut)) {
+
+
+            int oneByte = fis.read();
+
+            while (oneByte >= 0) {
+                if (oneByte == 32 || oneByte == 10) {
+                    fos.write(oneByte);
+                } else {
+                    String value = String.valueOf(oneByte);
+
+                    for (int i = 0; i < value.length(); i++) {
+                        fos.write(value.charAt(i));
+                    }
+                }
+
+                oneByte = fis.read();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
