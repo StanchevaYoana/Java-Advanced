7f2cebde01ebcc157bc4b8b1553caf57b8ca3547
StanchevaYoana <yoana.radoslavova@gmail.com>
2019-08-15 15:24:41 +0300

    Add task - Write to file .

---
+++ Streams, Files And Directories - Lab/src/WriteToFile.java
@@ -0,0 +1,30 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class WriteToFile {
+
+    public static void main(String[] args) {
+
+        String pathIn = System.getProperty("user.dir") + "/res/input.txt";
+        String pathOut = System.getProperty("user.dir") + "/res/output.txt";
+
+        try (FileInputStream fis = new FileInputStream(pathIn);
+             FileOutputStream fos = new FileOutputStream(pathOut)) {
+
+            int oneByte = fis.read();
+            while (oneByte >= 0) {
+                if (oneByte != '!' &&
+                        oneByte != '?' &&
+                        oneByte != '.' &&
+                        oneByte != ',') {
+                    fos.write(oneByte);
+                }
+                oneByte = fis.read();
+            }
+        } catch (
+                IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
