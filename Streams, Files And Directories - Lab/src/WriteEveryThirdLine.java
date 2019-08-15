4247b9c82bcd228e1328106673038680ae3b9b60
StanchevaYoana <yoana.radoslavova@gmail.com>
2019-08-15 16:33:26 +0300

    Add tasks - Write Every Third Line .

--- Streams, Files And Directories - Lab/src/WriteEveryThirdLine.java
+++ Streams, Files And Directories - Lab/src/WriteEveryThirdLine.java
@@ -1,11 +1,30 @@
+import java.io.*;
+
 public class WriteEveryThirdLine {
     public static void main(String[] args) {
 
-        String pathIn = System.getProperty("user.dir") + "input.txt";
-        String pathOut = System.getProperty("user.dir") + "05.WriteEveryThirdLineOutput.txt";
+        String pathIn = System.getProperty("user.dir") + "/res/input.txt";
+        String pathOut = System.getProperty("user.dir") + "/res/05.WriteEveryThirdLineOutput.txt";
 
 
+        int counter = 1;
 
+        try (BufferedReader read = new BufferedReader(new FileReader(pathIn));
+             BufferedWriter print = new BufferedWriter(new PrintWriter(pathOut))) {
+            String line = "";
+
+            while ((line = read.readLine()) != null) {
+
+                if (counter % 3 == 0) {
+                    print.write(line);
+                    print.write(System.lineSeparator());
+                }
+                counter++;
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-
 }
+
