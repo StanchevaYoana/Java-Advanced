606a1cd53441a12db50a05e018b219e2733c73a0
StanchevaYoana <yoana.radoslavova@gmail.com>
2019-08-16 14:21:35 +0300

    Add task - Add VAT .

--- Functional Programming - Lab/src/AddVAT.java
+++ Functional Programming - Lab/src/AddVAT.java
@@ -2,24 +2,16 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
 import java.util.function.UnaryOperator;
-import java.util.stream.Collectors;
 
 public class AddVAT {
     public static void main(String[] args) throws IOException {
         BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-
-        double[] numbers = Arrays.stream(read.readLine()
-                .split(", ")).mapToDouble(Double::parseDouble).toArray();
-
-
-        UnaryOperator<Double> addVat = t -> t * 1.2;
-        double[] vatIncluded = Arrays.stream(numbers).map(addVat::apply).toArray();
+        String[] numbers = read.readLine().split(", ");
+        UnaryOperator<Double> addVat = t -> 1.2 * t;
         System.out.println("Prices with VAT:");
-        for (double num : vatIncluded) {
-            System.out.printf("%.2f%n", num);
-        }
+        Arrays.stream(numbers).map(Double::parseDouble)
+                .map(addVat).forEach(n -> System.out.printf("%.2f%n", n));
+
     }
 }
