32691672c151213b17304bb3001340f8756e35c8
StanchevaYoana <yoana.radoslavova@gmail.com>
2019-08-16 13:40:27 +0300

    Add task .

--- Functional Programming - Lab/src/CountUppercaseWords.java
+++ Functional Programming - Lab/src/CountUppercaseWords.java
@@ -1,21 +1,19 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class CountUppercaseWords {
     public static void main(String[] args) throws IOException {
         BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
 
         String[] input = read.readLine().split("\\s+");
-        int count = 0;
-        StringBuilder words = new StringBuilder();
-        for (String word : input) {
-            if (Character.isAlphabetic(word.charAt(0)) && Character.isUpperCase(word.charAt(0))) {
-                count++;
-                words.append(word).append(System.lineSeparator());
-            }
-        }
-        System.out.println(count);
-        System.out.println(words);
+        Predicate<String> isUpperCase = word -> Character.isUpperCase(word.charAt(0));
+        List<String> newList = Arrays.stream(input).filter(isUpperCase).collect(Collectors.toList());
+        System.out.println(newList.size());
+        System.out.println(String.join("\n", newList));
     }
 }
