874351cb2559df1a967e2e4541573da2d185b519
StanchevaYoana <yoana.radoslavova@gmail.com>
2019-08-16 15:10:05 +0300

    Add task - Find Evens or Odds  .

---
+++ Functional Programming - Lab/src/FindEvensOrOdds.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class FindEvensOrOdds {
+    public static void main(String[] args) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+
+        String [] input  = read.readLine().split(" ");
+        String type = read.readLine();
+
+        int lowerBound = Integer.parseInt(input[0]);
+        int upperBound = Integer.parseInt(input[1]);
+        Predicate<Integer> byType = number1 -> number1 % 2 == 0;
+        if(type.equals("odd")){
+            byType = number -> number % 2 != 0;
+        }
+
+        IntStream.rangeClosed(lowerBound,upperBound)
+                .boxed()
+                .filter(byType)
+                .forEach(x -> System.out.print(x + " "));
+    }
+}
