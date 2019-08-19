99ae86caae51e4f40de66da817da4051aee0dbe0
StanchevaYoana <yoana.radoslavova@gmail.com>
2019-08-19 15:38:38 +0300

    Add task .

---
+++ Functional Programming - Exercises/src/KnightsOfHonor.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.function.Consumer;
+
+public class KnightsOfHonor {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+
+        Consumer<String> consumer = t -> System.out.println(String.format("Sir %s", t));
+
+        Arrays.stream(read.readLine().split("\\s+")).forEach(consumer);
+    }
+}
