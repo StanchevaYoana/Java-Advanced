8f7fce115814d64f49b79f5a5913647139bb63f8
StanchevaYoana <yoana.radoslavova@gmail.com>
2019-08-19 15:12:22 +0300

    Add task - Find Evens or Odds  .

--- Functional Programming - Lab/src/FindEvensOrOdds.java
+++ Functional Programming - Lab/src/FindEvensOrOdds.java
@@ -1,9 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.List;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class FindEvensOrOdds {
