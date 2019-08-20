b5a99787e2d5114d2b18d0df7fe535f2c12fe873
StanchevaYoana <yoana.radoslavova@gmail.com>
2019-08-20 16:49:22 +0300

    Add task - Custom comparator .

---
+++ Functional Programming - Exercises/src/CustomComparator.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CustomComparator {
+    public static void main(String[] args) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        List<Integer> nums = Arrays.stream(read.readLine().split("\\s+")).map(Integer::parseInt).collect(Collectors.toList());
+
+        Comparator<Integer> comparator = ((a, b) -> {
+            if (a % 2 == 0 && b % 2 != 0) {
+                return -1;
+            } else if (a % 2 != 0 && b % 2 == 0) {
+
+                return 1;
+            } else {
+                return a.compareTo(b);
+            }
+        });
+
+        nums.stream().sorted(comparator).forEach(num -> System.out.print(num + " "));
+    }
+}
