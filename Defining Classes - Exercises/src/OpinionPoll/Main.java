5bf88da3d749f9ec62e819632979edda00451800
StanchevaYoana <yoana.radoslavova@gmail.com>
2019-08-23 18:15:32 +0300

    Add task - Opinion poll .

--- Defining Classes - Exercises/src/OpinionPoll/Main.java
+++ Defining Classes - Exercises/src/OpinionPoll/Main.java
@@ -6,20 +6,18 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Map<String, OpinionPoll> opinionPollMap = new HashMap<String, OpinionPoll>();
+        List<OpinionPoll> pollList = new LinkedList<OpinionPoll>();
         int n = Integer.parseInt(scanner.nextLine());
         while (n-- > 0) {
             String[] data = scanner.nextLine().split("\\s+");
             int age = Integer.parseInt(data[1]);
             String name = data[0];
-            OpinionPoll opinionPoll = new OpinionPoll();
-            opinionPollMap.put(name, opinionPoll);
-            opinionPollMap.get(name).setName(name);
-            opinionPollMap.get(name).setAge(age);
+            OpinionPoll poll = new OpinionPoll(name, age);
+            pollList.add(poll);
         }
 
-        opinionPollMap.entrySet().stream().filter(age -> age.getValue().getAge() > 30).sorted(Comparator.comparing(Map.Entry::getKey))
-                .forEach((key -> System.out.println(String.format("%s - %d", key.getKey(), key.getValue().getAge()))));
+        pollList.stream().filter(person -> person.getAge() > 30).sorted(Comparator.comparing(OpinionPoll::getName))
+                .forEach((person -> System.out.println(String.format("%s - %d", person.getName(),person.getAge()))));
 
     }
 }
--- Defining Classes - Exercises/src/OpinionPoll/OpinionPoll.java
+++ Defining Classes - Exercises/src/OpinionPoll/OpinionPoll.java
@@ -5,6 +5,11 @@ public class OpinionPoll {
     private String name;
     private int age;
 
+    public OpinionPoll(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
     public String getName() {
         return name;
     }
