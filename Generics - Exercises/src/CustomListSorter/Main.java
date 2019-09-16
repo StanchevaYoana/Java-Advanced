bf32f60655d555519fa81d7c5858f8896f08d1d7
StanchevaYoana <yoana.radoslavova@gmail.com>
2019-09-16 17:14:44 +0300

    Add task .

---
+++ Generics - Exercises/src/CustomListSorter/Main.java
@@ -0,0 +1,50 @@
+package CustomListSorter;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+
+        CustomList<String> customList = new CustomList<>();
+
+        String input;
+
+        while(!"END".equals(input=read.readLine())){
+
+            String[] tokens = input.split("\\s+");
+
+            switch (tokens[0]){
+                case "Add":
+                    customList.add(tokens[1]);
+                    break;
+                case "Remove":
+                    customList.remove(Integer.parseInt(tokens[1]));
+                    break;
+                case "Contains":
+                    System.out.println(customList.contains(tokens[1]));
+                    break;
+                case "Swap":
+                    customList.swap(Integer.parseInt(tokens[1]),Integer.parseInt(tokens[2]));
+                    break;
+                case "Greater":
+                    System.out.println(customList.countGreaterThan(tokens[1]));
+                    break;
+                case "Max":
+                    System.out.println(customList.getMax());
+                    break;
+                case "Min":
+                    System.out.println(customList.getMin());
+                    break;
+                case "Print":
+                    customList.getData().forEach(System.out::println);
+                    break;
+                case "Sort":
+                    customList.sort();
+                    break;
+            }
+        }
+    }
+}
