2132c49ffcf15f2bb251559003f03978de600357
StanchevaYoana <yoana.radoslavova@gmail.com>
2019-09-17 15:45:16 +0300

    Add task - Comparable book .

---
+++ Iterators and Comparators - Lab/src/ComparableBook/Book.java
@@ -0,0 +1,54 @@
+package ComparableBook;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Book implements Comparable<Book> {
+    private String title;
+    private int year;
+    private List<String> authors;
+
+    public Book(String title, int year, String... authors) {
+        this.title = title;
+        this.year = year;
+        this.authors = Arrays.asList(authors);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public List<String> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(List<String> authors) {
+        this.authors = authors;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    @Override
+    public int compareTo(Book o1) {
+        if (this.title.compareTo(o1.getTitle()) == 0) {
+            return this.year - o1.getYear();
+        } else {
+            return this.title.compareTo(o1.getTitle());
+        }
+    }
+
+    @Override
+    public String toString() {
+        return this.getTitle();
+    }
+}
