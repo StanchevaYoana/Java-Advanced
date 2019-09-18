491baaaef6d85f61f30908692376150cd9df30a3
StanchevaYoana <yoana.radoslavova@gmail.com>
2019-09-18 11:11:23 +0300

    Add task - Book comparator .

---
+++ Iterators and Comparators - Lab/src/BookComparator/BookComparator.java
@@ -0,0 +1,10 @@
+package BookComparator;
+
+import java.util.Comparator;
+
+public class BookComparator implements Comparator<Book> {
+    @Override
+    public int compare(Book o1, Book o2) {
+        return o1.getTitle().compareTo(o2.getTitle());
+    }
+}
